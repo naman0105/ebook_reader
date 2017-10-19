c30c21aa13b8e05eb86cdea2b73996c558090799
naman bansal <naman0105@gmail.com>
2017-10-20 00:50:12 +0530

    dsf

--- js/first.js
+++ js/first.js
@@ -2,12 +2,13 @@
 
 	function selectBook(){
 		// var Book = ePub("epub.js-master/books/a.epub");
+		bookname = localStorage.getItem("book");
 		var Book = ePub("epub.js-master/books/"+ bookname);
 		Book.renderTo("area");
 	}
 
 
 	function setBookName(book){
-		bookname = book;
+		localStorage.setItem("book",book);
 		window.location.href = "https://naman0105.github.io/ebook_reader/readbook.html";
 	}
